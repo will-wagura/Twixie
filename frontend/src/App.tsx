13a4e8c29a3cbee22c212c6ee8e16adb2db1d90a
stephy <stephshiro35@gmail.com>
2024-07-12 05:50:00 +0300

    change pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Loading from "./Components/Loading";
-import Login from "./Components/Login";
-import Sidebar from "./Components/Sidebar";
-import Profile from "./Components/Profile";
-import Explore from "./Pages/Explore";
-import Home from "./Pages/Home";
-import Notifications from "./Pages/Notifications";
-import Messages from "./Pages/Messages";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import "./App.css"
+import Loading from "./components/Loading";
+import Login from "./components/Login";
+
+import Profile from "./pages/Profile";
+
+import Home from "./pages/Home";
 
 
 const App: React.FC = () => {
@@ -17,12 +16,10 @@ const App: React.FC = () => {
         <Route path="/" element={<Loading />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Sidebar />} />
+      
         
-        <Route path="/profile" element={<Profile userId={0} />} />
-        <Route path="/explore" element={<Explore/>} />
-        <Route path="/notifications" element={<Notifications/>} />
-        <Route path="/messages" element={<Messages/>} />
+        <Route path="/profile" element={<Profile  />} />
+       
       </Routes>
     </Router>
   );
